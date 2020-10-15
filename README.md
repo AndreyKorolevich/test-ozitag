1b4109d8babcf9e817fd007b0476c687e448a2e8
andreykorolevich <korolevich1994@mail.ru>
2020-10-15 18:27:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Тестовое задание
+
+**Необходимо сверстать трехуровневое фдаптивное меню.
+
+![](./img/Screenshot_1.png)
+![](./img/Screenshot_2.png)
+![](./img/Screenshot_3.png)
+![](./img/Screenshot_4.png)
+![](./img/Screenshot_5.png)
+![](./img/Screenshot_6.png)
---
+++ index.js
@@ -0,0 +1,27 @@
+const check = document.querySelector('.menu__check');
+const subs = document.querySelectorAll('.fa');
+
+check.addEventListener('click', (event) => {
+    const bars = document.getElementById('bars')
+    bars.classList.toggle('fa-times');
+    bars.classList.toggle('fas');
+    bars.classList.toggle('fa-bars');
+    bars.classList.toggle('fa');
+})
+
+subs.forEach(e => e.addEventListener('click', (event) => {
+    const parent = event.target.closest('ul').classList.value;
+    const sub = document.querySelectorAll(`.${parent} > li > .first-floor__sub`);
+    sub.forEach(e => {
+        const targetInput = event.target.closest('li').firstElementChild;
+
+        console.log(e.id, targetInput.id, e.checked)
+        if (e.id !== targetInput.id && e.id.trim() !== '' && e.checked) {
+            e.checked = !e.checked;
+            const event = new Event('change');
+            e.dispatchEvent(event);
+        }
+    })
+    event.target.scrollIntoView();
+}))
+
